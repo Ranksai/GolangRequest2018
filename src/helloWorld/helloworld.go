35c3557d26619351d2eb745d19e4f6fc550e4600
Ranksai <ranks.j.inter1908@gmail.com>
2018-02-15 11:47:40 +0900

    add variable value

--- src/helloWorld/helloworld.go
+++ src/helloWorld/helloworld.go
@@ -1,10 +1,11 @@
-/*
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("hello, 世界")
+	// var helloWorld string = "hello, 世界"
+	helloWorld := "hello, 世界"
+
+	fmt.Println(helloWorld)
 	fmt.Println("hello, world")
 }
-*/
