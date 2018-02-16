586fbaaf76dd924457be4285114e68bb01d1ce74
Ranksai <ranks.j.inter1908@gmail.com>
2018-02-16 12:24:17 +0900

    add flag

---
+++ src/day1/flag/flag.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+// 設定される変数のポインタを取得
+var msg = flag.String("msg", "デフォルト値", "使い方")
+var n int
+
+func init() {
+	// ポインタを指定して設定を予約
+	flag.IntVar(&n, "n", 1, "回数")
+}
+func main() {
+	// ここで実際に設定される
+	flag.Parse()
+	fmt.Println(strings.Repeat(*msg, n))
+}
