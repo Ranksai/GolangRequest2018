9e2b30da98efb6da2ab3b579873cfcc90c85fa64
Ranksai <ranks.j.inter1908@gmail.com>
2018-02-16 13:49:32 +0900

    add mycat

---
+++ src/day1/mycat/mycat.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+)
+
+var lineNumber = 1
+var isLineNumber bool
+
+func init() {
+	flag.BoolVar(&isLineNumber, "n", false, "usage: -n")
+}
+
+func openFile(filename string) (*os.File, error) {
+	sf, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	return sf, nil
+}
+
+func printFile(scanFiles []*os.File, isLineNumber bool) {
+	for i := range scanFiles {
+		scanner := bufio.NewScanner(scanFiles[i])
+		for scanner.Scan() {
+			if isLineNumber {
+				fmt.Printf("%v: ", lineNumber)
+				lineNumber++
+			}
+			fmt.Println(scanner.Text())
+		}
+	}
+}
+
+func myCat(args []string, isLineNumber bool) {
+	var filePaths []string
+	var sfs []*os.File
+
+	// check args
+	for i := 1; i < len(args); i++ {
+		if flag := string([]rune(args[i])[:1]); flag != "-" {
+			filePaths = append(filePaths, args[i])
+		} else {
+			switch args[i] {
+			case "-n":
+				isLineNumber = true
+			}
+		}
+	}
+
+	for i := range filePaths {
+		sf, err := openFile(filePaths[i])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s can not open !\n", filePaths[i])
+		} else {
+			sfs = append(sfs, sf)
+		}
+		defer sf.Close()
+	}
+
+	printFile(sfs, isLineNumber)
+}
+
+func main() {
+	// flag
+	flag.Parse()
+	myCat(os.Args, isLineNumber)
+}
