e2877d23e63bec42c5170a77fc0d5495e9173e25
Ranksai <ranks.j.inter1908@gmail.com>
2018-02-17 18:42:41 +0900

    add score and TIMEOUT

--- src/day2HomeWork/typingGame/typingGame.go
+++ src/day2HomeWork/typingGame/typingGame.go
@@ -8,6 +8,8 @@ import (
 	"time"
 )
 
+const TIMEOUT = 10
+
 var words = []string{"dog", "cat"}
 
 func input(r io.Reader) <-chan string {
@@ -25,7 +27,7 @@ func input(r io.Reader) <-chan string {
 func timeout() <-chan string {
 	ch := make(chan string)
 	go func() {
-		time.Sleep(30 * time.Second)
+		time.Sleep(TIMEOUT * time.Second)
 		ch <- "timeout"
 	}()
 	return ch
@@ -38,23 +40,28 @@ func printQuiz() string {
 
 func TypingGame() {
 
+	var score int
 	ch := input(os.Stdin)
 	chTime := timeout()
 	for {
-
 		quiz := printQuiz()
 		fmt.Print(">")
 		select {
+		// receive typed string
 		case receive := <-ch:
 			if quiz == receive {
 				fmt.Println("correct")
+				score++
 			} else {
 				fmt.Println("not correct")
 			}
+			// timeout typing
 		case <-time.After(10 * time.Second):
 			fmt.Println("not correct")
+			// timeout game
 		case timeout := <-chTime:
 			fmt.Printf("\n%s\n", timeout)
+			fmt.Printf("your score is %d !!!\n", score)
 			return
 		}
 	}
